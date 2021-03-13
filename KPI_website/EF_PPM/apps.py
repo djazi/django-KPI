618f27b397018619af8f71d0d5770ab8a0702d84
djazi <52473469+djazi@users.noreply.github.com>
2021-03-13 23:11:54 +0100

    first commit

---
+++ KPI_website/EF_PPM/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EfPpmConfig(AppConfig):
+    name = 'EF_PPM'
---
+++ KPI_website/EF_PPM/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+app_name = "EF_PPM"
+urlpatterns = [
+    path('',views.index,name='KPI'),
+    path('table',views.table,name="table")
+]
