77076471f6e27bec76c3b99d5f7021dd2a123e4b
djazi <52473469+djazi@users.noreply.github.com>
2021-03-20 14:35:18 +0100

    play columns on django/admin

--- KPI_website/EF_PPM/admin.py
+++ KPI_website/EF_PPM/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from . models import production, production_NC, KPI
+from . models import * #production, production_NC, KPI
 # Register your models here.
-admin.site.register(production)
-admin.site.register(production_NC)
-admin.site.register(KPI)
+
+
+class productionAdmin(admin.ModelAdmin):
+    list_display = ("ID_prod", "code_article", "Date", "heure")
+
+class production_NCAdmin(admin.ModelAdmin):
+    list_display = ("ID_prod_NC", "code_article", "Date", "heure")
+class KPIAdmin(admin.ModelAdmin):
+    list_display = ("ID_per", "Date", "heure", "EFF_h", "PPM_h", "EFF_j", "PPM_j")
+                    
+                    
+                   
+
+admin.site.register(production, productionAdmin )
+admin.site.register(production_NC, production_NCAdmin)
+admin.site.register(KPI, KPIAdmin)
 
