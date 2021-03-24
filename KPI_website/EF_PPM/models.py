55b8f02b5610b7f62fd1da897fddf4c41c6abee4
djazi <52473469+djazi@users.noreply.github.com>
2021-03-24 04:51:18 +0100

    another step

--- KPI_website/EF_PPM/models.py
+++ KPI_website/EF_PPM/models.py
@@ -1,3 +1,4 @@
+from enum import auto
 from django.db import models
 
 # Create your models here.
@@ -24,13 +25,13 @@ class production_NC(models.Model):
         return f"{self.ID_prod_NC} {self.code_article} {self.Date} {self.heure}"
 
 class KPI(models.Model):
-    ID_per = models.CharField(max_length=100)
+    ID_per = models.AutoField(primary_key=True)
     Date = models.CharField(max_length=100)
-    heure = models.TimeField()
-    EFF_h = models.CharField(max_length=100)
-    PPM_h = models.CharField(max_length=100)
-    EFF_j = models.CharField(max_length=100)
-    PPM_j = models.CharField(max_length=100)
+    heure = models.TimeField(null=True)
+    EFF_h = models.FloatField(null=True)
+    PPM_h = models.FloatField(null=True)
+    EFF_j = models.FloatField(null=True)
+    PPM_j = models.FloatField(null=True)
 
     def __str__(self):
         return f"{self.ID_per} {self.Date} {self.heure} {self.EFF_h}  {self.PPM_h} {self.EFF_j} {self.PPM_j} "
--- KPI_website/EF_PPM/views.py
+++ KPI_website/EF_PPM/views.py
@@ -1,25 +1,42 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from . models import *
 from django.http import HttpResponseBadRequest, HttpResponseRedirect, Http404, request
 from django.urls import reverse
 from django.db.models import Count
+from datetime import datetime, timedelta
 # Create your views here.
 def index(request):
 
-    
-    
     date = production.objects.distinct("Date").all()
-    eff= production.objects.all().count()
+    eff = production.objects.all().count()
     ppm = production_NC.objects.all().count()
-    pc=  round((eff/120)*100,2)
-    pnc=round((ppm/eff)*1000000,2)
+    pc = round((eff/120)*100, 2)
+    pnc = round((ppm/eff)*1000000, 2)
 
 
-    return render(request, 'index.html', {
-        "eff": pc,
-        "ppm":pnc,
-        "dates":date
-    })
+    
+    
+    
+    if request.method == 'POST' and 'snap' in request.POST:
+        
+        h=request.POST['heure']
+        d = datetime.strptime(h,'%H:%M') - timedelta(hours=1)
+        j = production.objects.filter(Date='20/03/2021',heure__lte = h ,heure__gte=d.strftime('%H:%M')).count()
+        di = request.POST['datesss']
+
+        
+        return render(request, 'index.html', {"eff": pc, "ppm": pnc,"dates": date,"heu":j,"hi":d.strftime('%H:%M')})
+
+
+    if request.method == 'POST' and 'save' in request.POST:
+        k = KPI(EFF_h=pc, PPM_h=pnc, Date='20/03/2021', heure=request.POST['heure'])
+        k.save()
+        return render(request, 'index.html', {"eff": pc, "ppm": pnc, "dates": date})
+    
+
+
+    return render(request, 'index.html', {"dates": date})
 
 
 def table(request):
